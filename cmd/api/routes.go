008f60bfcecee83424a1d1bb14dd253efdf4c622
Patrik <patriknilsson1992@gmail.com>
2025-06-08 16:21:38 +0200

    Adds cors

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -3,7 +3,9 @@ package main
 import (
 	"net/http"
 	"rest-api-in-gin/internal/env"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -11,6 +13,15 @@ import (
 
 func (app *application) routes() http.Handler {
 	g := gin.Default()
+
+	config := cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
+	g.Use(cors.New(config))
 
 	v1 := g.Group("/api/v1")
 	{
